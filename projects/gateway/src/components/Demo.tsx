c9a844db700402ff57b232f0ba90ca3ead62a5c5
Alexander Munko <seasle98@gmail.com>
2024-12-15 18:11:31 +0700

    Изменяет открытие демок в Gateway

--- projects/gateway/src/components/Demo.tsx
+++ projects/gateway/src/components/Demo.tsx
@@ -2,7 +2,7 @@ import { useLocation } from 'react-router';
 
 export const Demo = () => {
   const { pathname } = useLocation();
-  const href = `/demo${pathname}/index.html`;
+  const href = `./demo${pathname}/index.html`;
 
   return <iframe style={{ width: '100vw', height: '100vh', border: 'none' }} src={href}></iframe>;
 };

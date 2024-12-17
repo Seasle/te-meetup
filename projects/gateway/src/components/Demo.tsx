60055b580988015ecb4726c54254075ba2c49267
Alexander Munko <seasle98@gmail.com>
2024-12-17 22:40:08 +0700

    Добавляет заголовок страницы в демках

--- projects/gateway/src/components/Demo.tsx
+++ projects/gateway/src/components/Demo.tsx
@@ -1,8 +1,17 @@
 import { useLocation } from 'react-router';
 
+import { routes } from '../constants/routes';
+
 export const Demo = () => {
   const { pathname } = useLocation();
   const href = `./demo${pathname}/index.html`;
 
-  return <iframe style={{ width: '100vw', height: '100vh', border: 'none' }} src={href}></iframe>;
+  const route = routes.find((route) => route.path === pathname);
+
+  return (
+    <>
+      <title>{route?.name}</title>
+      <iframe style={{ width: '100vw', height: '100vh', border: 'none' }} src={href}></iframe>
+    </>
+  );
 };
